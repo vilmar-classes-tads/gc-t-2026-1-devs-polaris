d4201784026d0e0d305d54129c824db806c9aec7
Stella <gregovichstella04@gmail.com>
2026-06-15 18:58:20 +0000

    feat: implementa cadastro de projeto

---
+++ src/main/java/br/ifpe/proext/model/Projeto.java
@@ -0,0 +1,60 @@
+package br.ifpe.proext.model;
+
+import java.util.ArrayList;
+
+public class Projeto {
+    private String titulo;
+    private String resumo;
+    private ArrayList<String> palavrasChave = new ArrayList<>();
+    private String publicoAlvo;
+    private String areaTematica;
+    private String campus;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+
+    public ArrayList<String> getPalavrasChave() {
+        return palavrasChave;
+    }
+
+    public void setPalavrasChave(ArrayList<String> palavrasChave) {
+        this.palavrasChave = palavrasChave;
+    }
+
+    public String getPublicoAlvo() {
+        return publicoAlvo;
+    }
+
+    public void setPublicoAlvo(String publicoAlvo) {
+        this.publicoAlvo = publicoAlvo;
+    }
+
+    public String getAreaTematica() {
+        return areaTematica;
+    }
+
+    public void setAreaTematica(String areaTematica) {
+        this.areaTematica = areaTematica;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+}
